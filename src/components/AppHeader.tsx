188ee3665d4951cf18d33820aef14b9439d37528
Khaled Mahmoud Abu-3rida <khaledabuarida0@gmail.com>
2024-01-31 23:17:35 +0200

    handle nav links

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,101 +1,80 @@
-import { AppBar, Container, Toolbar, Typography, Box, IconButton, Menu, MenuItem, Button, Badge } from "@mui/material";
-import {useContext, useState} from "react";
+import { AppBar, Container, Toolbar, Box, Badge, Button } from "@mui/material";
+import {useContext} from "react";
 import StorefrontIcon from '@mui/icons-material/Storefront';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import MenuIcon from '@mui/icons-material/Menu';
 import { NavLink } from "react-router-dom";
 import { CartContext } from "../contexts/Cart.context";
 
-
-const pages = ['Products', 'Pricing'];
-
 const AppHeader = () => {
 
-    const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
     const cartItems = useContext(CartContext)
 
-    const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-        setAnchorElNav(event.currentTarget);
-    };
-
-    const handleCloseNavMenu = () => {
-        setAnchorElNav(null);
-    };
     
     return (
     <AppBar position="static" sx={{ backgroundColor: "#1D2B53"}}>
       <Container>
-        <Toolbar disableGutters>
-            <StorefrontIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+        <Toolbar disableGutters sx={{ display: 'flex', justifyContent: 'space-between'}}>
             <NavLink 
                 to='/' 
                 style={{
                     textDecoration: 'none', 
-                    color: '#fff', 
+                    color: '#FFF', 
                     fontFamily: 'monospace', 
-                    fontSize: '24px'
+                    fontSize: '28px',
+                    fontWeight: 'bolder',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center'
                 }}
             >
+                <StorefrontIcon sx={{ fontSize: '28px', display: { xs: 'none', md: 'flex' }, mr: 1 }} />
                 Retina
             </NavLink>
 
-            <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-                <IconButton
-                    size="large"
-                    aria-label="account of current user"
-                    aria-controls="menu-appbar"
-                    aria-haspopup="true"
-                    onClick={handleOpenNavMenu}
-                    color="inherit"
-                >
-                <MenuIcon />
-                    </IconButton>
-                <Menu
-                    id="menu-appbar"
-                    anchorEl={anchorElNav}
-                    anchorOrigin={{
-                        vertical: 'bottom',
-                        horizontal: 'left',
-                    }}
-                    keepMounted
-                    transformOrigin={{
-                        vertical: 'top',
-                        horizontal: 'left',
-                    }}
-                    open={Boolean(anchorElNav)}
-                    onClose={handleCloseNavMenu}
-                    sx={{
-                        display: { xs: 'block', md: 'none' },
+           {/* NAV ITEMS */}
+            <Box>
+                <NavLink
+                    to='/'
+                    style={{
+                        textDecoration: 'none',
+                        color: '#FFF',
+                        fontSize: '20px',
                     }}
                 >
-                {pages.map((page) => (
-                    <MenuItem key={page} onClick={handleCloseNavMenu}>
-                        <Typography textAlign="center" >{page}</Typography>
-                    </MenuItem>
-                ))}
-                </Menu>
-            </Box>
-           
-            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-                {pages.map((page) => (
-                <Button
-                    key={page}
-                    onClick={handleCloseNavMenu}
-                    sx={{ my: 2, color: 'white', display: 'block', fontFamily: 'monospace'}}
+                    <Button sx={{ textTransform: 'none', color: 'inherit'}}>
+                        Products
+                    </Button>
+                </NavLink>
+                <NavLink
+                    to='/'
+                    style={{
+                        textDecoration: 'none',
+                        color: '#FFF',
+                        fontSize: '20px',
+                    }}
                 >
-                    {page}
-                </Button>
-                ))}
+                    <Button sx={{ textTransform: 'none', color: 'inherit'}}>
+                        Add Product
+                    </Button>
+                </NavLink>
+                
             </Box>
 
-            <NavLink to='/cart' style={{color: 'white', marginRight: '20px'}}>
-            <Badge badgeContent={cartItems?.cartItems.length} color="primary">
-                <ShoppingCartIcon/>
-            </Badge>
-            </NavLink>
+            <Box sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                }}
+            >
+                <NavLink to='/cart' style={{color: 'white', marginRight: '10px'}}>
+                    <Badge badgeContent={cartItems?.cartItems.length} color="primary">
+                        <ShoppingCartIcon/>
+                    </Badge>
+                </NavLink>
+                <p> User Name </p>
+            </Box>
            
-
-            <p> User Name </p>
+            
 
           
         </Toolbar>
